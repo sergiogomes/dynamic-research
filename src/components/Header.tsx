5d35273d163b11bd44f8ae6683bbd205897fd42c
Josimar16 <josimarjr479@gmail.com>
2021-06-17 22:00:25 -0300

    feat: :sparkles: Criando profile no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export const Header = () => {
   return (
@@ -54,6 +54,53 @@ export const Header = () => {
           as={RiSearchLine}
           fontSize="20"
         />
+      </Flex>
+
+      <Flex
+        alignItems="center"
+        marginLeft="auto"
+      >
+        <HStack
+          spacing="8"
+          marginX="8"
+          paddingRight="8"
+          paddingY="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon
+            as={RiNotificationLine}
+            fontSize="20"
+          />
+          <Icon
+            as={RiUserAddLine}
+            fontSize="20"
+          />
+        </HStack>
+
+        <Flex
+          alignItems="center"
+        >
+          <Box
+            marginRight="4"
+            textAlign="right"
+          >
+            <Text>Josimar Junior</Text>
+            <Text
+              color="gray.300"
+              fontSize="small"
+            >
+              josimarjr479@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Josimar Martins"
+            src="https://avatars.githubusercontent.com/u/49077388?s=400&u=ec9520ac11646eea256440b5db57ede4af4bf6be&v=4"
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
