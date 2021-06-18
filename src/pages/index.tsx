7d581e4b15d44b272f81e29b22acc5a3915fc75a
Josimar16 <josimarjr479@gmail.com>
2021-06-17 21:01:03 -0300

    feat: :sparkles: Criando tela de login
    Criando tela de login com chakra UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,75 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
 export default function Home() {
   return (
-    <h1>Hello World, DashGo!</h1>
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        backgroundColor="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack
+          spacing="4"
+        >
+          <FormControl>
+            <FormLabel
+              htmlFor="email"
+            >
+              E-mail
+            </FormLabel>
+
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              backgroundColor="gray.900"
+              focusBorderColor="pink.500"
+              variant="filled"
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel
+              htmlFor="password"
+            >
+              Senha
+            </FormLabel>
+
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              backgroundColor="gray.900"
+              focusBorderColor="pink.500"
+              variant="filled"
+              _hover={{
+                backgroundColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
