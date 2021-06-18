d7526fa93a7897d45f53e97a8e7d04db103df7b6
Josimar16 <josimarjr479@gmail.com>
2021-06-17 21:43:20 -0300

    feat: :sparkles: Criando componente de Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import { Flex, Input, Text, Icon } from '@chakra-ui/react';
+import { RiSearchLine } from 'react-icons/ri';
+
+export const Header = () => {
+  return (
+    <Flex
+      as="header"
+      width="100%"
+      maxWidth={1480}
+      height="20"
+      marginX="auto"
+      marginTop="4"
+      paddingX="6"
+      alignItems="center"
+    >
+      <Text
+        fontSize="3xl"
+        fontWeight="bold"
+        letterSpacing="tight"
+        width="64"
+      >
+        dashgo
+        <Text
+          as="span"
+          marginLeft="1"
+          color="pink.500"
+        >
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        paddingY="4"
+        paddingX="8"
+        marginLeft="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        backgroundColor="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          paddingX="4"
+          marginRight="4"
+          placeholder="Buscar na plataforma"
+          _placeholder={{ color: "gray.400" }}
+        />
+        <Icon
+          as={RiSearchLine}
+          fontSize="20"
+        />
+      </Flex>
+    </Flex>
+  );
+}
