cef666123934f6153be0a266f42cf75482c2a2f0
Josimar16 <josimarjr479@gmail.com>
2021-07-23 23:20:08 -0300

    feat: :sparkles: Criando rota de buscar o usuário pelo id

--- src/services/hooks/useUser.ts
+++ src/services/hooks/useUser.ts
@@ -8,8 +8,19 @@ interface User {
   createdAt: string;
 }
 
-export async function getUsers(): Promise<User[]> {
-  const { data } = await api.get('/users')
+interface GetUsersResponse {
+  users: User[];
+  totalCount: number;
+}
+
+export async function getUsers(page: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('/users', {
+    params: {
+      page
+    }
+  });
+
+  const totalCount = Number(headers['x-total-count']);
 
   const users = data.users.map((user: User) => {
     return {
@@ -24,11 +35,14 @@ export async function getUsers(): Promise<User[]> {
     }
   });
 
-  return users;
+  return {
+    users, 
+    totalCount
+  };
 }
 
-export function useUsers() {
-  return useQuery<User[]>('users', getUsers, {
-    staleTime: 1000 * 5, // 5 seconds
+export function useUsers(page: number) {
+  return useQuery<GetUsersResponse>(['users', page], () => getUsers(page), {
+    staleTime: 1000 * 60 * 10, // 10 minutos
   })
 }
