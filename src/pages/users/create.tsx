98ef8e16da285ef44e3c7c802b2ba3d1f3ea63fe
Josimar16 <josimarjr479@gmail.com>
2021-07-24 09:44:59 -0300

    feat: :sparkles: Criando funcionalidade de criar usuário e de invalidar cache

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -3,10 +3,15 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import Link from 'next/link';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
+import { useRouter } from 'next/router';
+import { useMutation } from 'react-query';
+
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Heading } from '../../components/Heading';
 import { Sidebar } from '../../components/Sidebar';
+import { api } from '../../services/api';
+import { queryClient } from '../../services/queryClient';
 
 
 type CreateUserFormData = {
@@ -26,12 +31,30 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    });
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+    }
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   });
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (data) => {
-
+    await createUser.mutateAsync(data);
+    router.push('/users');
   }
 
   return (
