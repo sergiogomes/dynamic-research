5ae22d43a16821e408294f875c94df3f203c949a
Josimar16 <josimarjr479@gmail.com>
2021-07-23 23:17:37 -0300

    refactor: :recycle: retirando o queryCliente do app e colocando em um arquivo global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
-import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../styles/theme';
-
+import { AppProps } from 'next/app';
+import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../hooks/SidebarDrawer';
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
+import { theme } from '../styles/theme';
+
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
