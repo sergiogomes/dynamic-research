4e3221d528a5de5aa39baaeca851449fff23f763
sergiogomes <sergiopgomes92@gmail.com>
2022-06-18 18:39:49 -0300

    fix: :pencil2: change user endpoint

--- src/pages/usuarios/adicionar.tsx
+++ src/pages/usuarios/adicionar.tsx
@@ -1,10 +1,11 @@
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import * as yup from 'yup';
+
 import { Box, Button, Divider, Flex, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
 import { yupResolver } from '@hookform/resolvers/yup';
-import Link from 'next/link';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { useRouter } from 'next/router';
-import { useMutation } from 'react-query';
 
 import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
@@ -12,7 +13,6 @@ import { Heading } from '../../components/Heading';
 import { Sidebar } from '../../components/Sidebar';
 import { api } from '../../services/api';
 import { queryClient } from '../../services/queryClient';
-
 
 type CreateUserFormData = {
   name: string;
@@ -34,7 +34,7 @@ export default function CreateUser() {
   const router = useRouter();
 
   const createUser = useMutation(async (user: CreateUserFormData) => {
-    const response = await api.post('users', {
+    const response = await api.post('usuarios', {
       user: {
         ...user,
         created_at: new Date(),
@@ -44,7 +44,7 @@ export default function CreateUser() {
     return response.data.user;
   }, {
     onSuccess: () => {
-      queryClient.invalidateQueries('users');
+      queryClient.invalidateQueries('usuarios');
     }
   });
 
